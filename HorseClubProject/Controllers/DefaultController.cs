f5dcfe76d14724977a15bf2c20ee09299fd47a97
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-03-20 15:31:12 +0300

    Add project files.

---
+++ HorseClubProject/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HorseClubProject.Models.Entity;
+
+namespace HorseClubProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HorseDbEntities db=new HorseDbEntities();
+        public ActionResult Index()
+        {
+            var values = db.TBLBILGI.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ HorseClubProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HorseClubProject.MvcApplication" Language="C#" %>
